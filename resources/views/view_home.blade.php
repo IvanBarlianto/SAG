2559bd8e93a31dccdd84db46d48c5bb397289c5d
IvanBarlianto <ivanasshidiqqi@gmail.com>
2024-05-30 13:09:55 +0700

    first commit

---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,26 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+        Perpustakaan Perguruan Tinggi merupakan unit kerja pelaksana teknis (UPT) Perguruan Tinggi yang bersama-sama
+		dengan unit lain turut melaksanakan Tri Dharma Perguruan Tinggi, yaitu: pendidikan, penelitian, dan pengabdian
+		kepada masyarakat dengan cara memilah, menghimpun, mengolah, merawat, serta menyebarluaskan sumber informasi kepada
+		lembaga induknya pada khususnya dan sivitas akademika pada umumnya. Kelima tugas tersebut dilaksanakan dengan
+		tata cara administrasi dan organisasi yang berlaku bagi penyelenggaraan sebuah perpustakaan.
+    <p>
+    <h4>Perpustakaan SAG</h4>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consectetur vehicula congue. Praesent nec augue at nisl
+    scelerisque ultrices a a mauris. Mauris condimentum turpis ligula, eleifend consectetur velit eleifend porta. Praesent
+    eu neque bibendum, pretium est ac, pulvinar enim. Sed rutrum vestibulum posuere. Aenean faucibus mauris nibh, nec lobortis
+    urna lobortis in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed egestas
+    nibh erat, ac vulputate eros gravida ut. In iaculis iaculis blandit. Curabitur a dignissim sem, eu iaculis ex. Vestibulum in
+    lacinia tortor. Donec ornare velit sit amet ante facilisis, eget accumsan dui auctor.
+    <p>
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+    </p>
+@endsection
