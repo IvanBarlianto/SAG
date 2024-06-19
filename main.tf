9e2ecf81ebdb9e2f156bd1c546647cd0220ca228
DylanKaisar <112993700+DylanKaisar@users.noreply.github.com>
2024-06-19 16:40:02 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,6 @@ resource "aws_security_group" "allow_http_ssh" {
 resource "aws_instance" "public_instance" {
   ami                    = "ami-04b70fa74e45c3917"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.service_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
 
   tags = {
