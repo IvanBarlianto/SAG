84dc1e9845d6f57a857918c3169e095eee1259ba
DylanKaisar <112993700+DylanKaisar@users.noreply.github.com>
2024-06-19 16:22:43 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region                  = "us-east-1"
-  access_key              = "AKIA2UC3CNSNSB2XARUB"
-  secret_key              = "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"
+  # access_key              = "AKIA2UC3CNSNSB2XARUB"
+  # secret_key              = "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"
 }
 
 resource "tls_private_key" "rsa_4096" {
@@ -65,17 +65,17 @@ resource "aws_security_group" "allow_http_ssh" {
   }
 }
 
-resource "aws_instance" "public_instance" {
-  ami                    = "ami-04b70fa74e45c3917"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.service_key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+# resource "aws_instance" "public_instance" {
+#   ami                    = "ami-04b70fa74e45c3917"
+#   instance_type          = "t2.micro"
+#   key_name               = aws_key_pair.service_key_pair.key_name
+#   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+#
+#   tags = {
+#     Name = "instance_terraform_2"
+#   }
+# }
 
-  tags = {
-    Name = "instance_terraform_2"
-  }
-}
-
-output "instance_ip" {
-  value = aws_instance.public_instance.public_ip
-}
+# output "instance_ip" {
+#   value = aws_instance.public_instance.public_ip
+# }
